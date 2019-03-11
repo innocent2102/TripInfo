74859fcc7704801c13403b808720cb939f4f9f81
gorlewski.michal <michal2102@gmail.com>
2019-03-11 14:30:13 +0100

    move firebase config from module to environment and add getting sample trip collection

---
+++ src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDdNREjKIVCRtzapsMT_alhI8uxMyg7JFs',
+    authDomain: 'tripinfo-d906f.firebaseapp.com',
+    databaseURL: 'https://tripinfo-d906f.firebaseio.com',
+    projectId: 'tripinfo-d906f',
+    storageBucket: 'tripinfo-d906f.appspot.com',
+    messagingSenderId: '49799060798'
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
