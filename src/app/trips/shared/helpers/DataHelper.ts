5fece961f991e01935799389c0059b877ab2be60
m.gorlewski <m.gorlewski@nowaera.pl>
2019-12-08 20:02:44 +0100

    feat: add getRandomInt helper

--- src/app/trips/shared/helpers/DataHelper.ts
+++ src/app/trips/shared/helpers/DataHelper.ts
@@ -6,4 +6,11 @@ export class DataHelper {
   static hasAnyElement(obj: any[]): boolean {
     return this.hasValue(obj) && obj.length > 0;
   }
+
+  // Returns a random integer between min (inclusive) and max (inclusive).
+  static getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
 }
