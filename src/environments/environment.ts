5b3acbfbca45226e62b8bdb9c549c7f810f0df27
m.gorlewski <m.gorlewski@nowaera.pl>
2019-12-08 20:03:23 +0100

    feat: change environment to non-production

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
+  production: false,
   firebaseConfig: {
     apiKey: 'AIzaSyDdNREjKIVCRtzapsMT_alhI8uxMyg7JFs',
     authDomain: 'tripinfo-d906f.firebaseapp.com',
